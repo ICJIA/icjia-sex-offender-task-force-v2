ab991a0c15d189e622370df61e65f88432c17674
Chris Schweda <cschweda@gmail.com>
2017-02-10 12:39:55 -0600

    added favicons to /static
    Fixed a http-->https error when meta tags were fetching favicons from
    non-https server

--- src/meta/index.js
+++ src/meta/index.js
@@ -11,10 +11,10 @@ module.exports = {
       // { rel: 'stylesheet', src: 'https://fonts.googleapis.com/css?family=Montserrat:400,700' },
       // { rel: 'stylesheet', src: 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800' },
 
-    {rel: "icon", type: "image/png", sizes: "192x192",  href: 'http://www.icjia.state.il.us/_themes/icjia/img/icons/android-icon-192x192.png?v=1440170181'},
-    {rel: "icon", type: "image/png", sizes: "32x32",  href: 'http://www.icjia.state.il.us/_themes/icjia/img/icons/favicon-32x32.png?v=1440170181'},
-    {rel: "icon", type: "image/png", sizes: "96x96",  href: 'http://www.icjia.state.il.us/_themes/icjia/img/icons/favicon-96x96.png?v=1440170181'},
-    {rel: "icon", type: "image/png", sizes: "16x16",  href: 'http://www.icjia.state.il.us/_themes/icjia/img/icons/favicon-16x16.png?v=1440170181'}
+    {rel: "icon", type: "image/png", sizes: "192x192",  href: '/static/icons/android-icon-192x192.png?v=1440170181'},
+    {rel: "icon", type: "image/png", sizes: "32x32",  href: '/static/icons/favicon-32x32.png?v=1440170181'},
+    {rel: "icon", type: "image/png", sizes: "96x96",  href: '/static/icons/favicon-96x96.png?v=1440170181'},
+    {rel: "icon", type: "image/png", sizes: "16x16",  href: '/static/icons/favicon-16x16.png?v=1440170181'}
 
 
 

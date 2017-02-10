7ce2f98045621c0f56cc6d5fcf61660caa21ae59
Chris Schweda <cschweda@gmail.com>
2017-02-10 13:52:37 -0600

    moved google meta info to /meta

--- src/meta/index.js
+++ src/meta/index.js
@@ -1,23 +1,23 @@
 module.exports = {
   meta: [
     { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { property: "og:title", content: "Illinois Sex Offenders & Offender Registration Task Force" },
+    { property: "og:site_name", content: "Illinois Sex Offenders & Offender Registration Task Force" },
+    { property: "og:url", content: "https://soortf.icjia.cloud" },
+    { property: "og:description", content: "Illinois Sex Offenders & Offender Registration Task Force. "},
+    { property: "fb:app_id", content: "963916987072512" },
+    { property: "og:type", content: "article" },
+    { property: "og:locale", content: "en_US" },
+    { property: "article:author", content: "https://www.facebook.com/ICJIA" },
+    { property: "og:image", content: "https://soortf.icjia.cloud/static/facebook/soortf-small.jpg" },
   ],
-  script: [
-      // { src: 'https://use.fontawesome.com/0fc34dc4bc.js', type: 'text/javascript'}
-    ],
+  script: [],
   link: [
-      // { rel: 'stylesheet', src: 'https://fonts.googleapis.com/css?family=Lato:400,700,900|Roboto:400,500,700,900' },
-      // { rel: 'stylesheet', src: 'https://fonts.googleapis.com/css?family=Montserrat:400,700' },
-      // { rel: 'stylesheet', src: 'https://fonts.googleapis.com/css?family=Open+Sans:300,400,700,800' },
-
     {rel: "icon", type: "image/png", sizes: "192x192",  href: '/static/icons/android-icon-192x192.png?v=1440170181'},
     {rel: "icon", type: "image/png", sizes: "32x32",  href: '/static/icons/favicon-32x32.png?v=1440170181'},
     {rel: "icon", type: "image/png", sizes: "96x96",  href: '/static/icons/favicon-96x96.png?v=1440170181'},
     {rel: "icon", type: "image/png", sizes: "16x16",  href: '/static/icons/favicon-16x16.png?v=1440170181'}
-
-
-
     ],
     // if no subcomponents specify a metaInfo.title, this title will be used
     title: 'Illinois Sex Offenders & Offender Registration Task Force',

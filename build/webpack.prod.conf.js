fb61932d84da4340db322c7073510756f2538547
Chris Schweda <cschweda@gmail.com>
2018-04-26 09:34:56 -0500

    Update prerender plugin

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,22 +1,21 @@
-var path = require('path')
-var utils = require('./utils')
-var webpack = require('webpack')
-var config = require('../config')
-var merge = require('webpack-merge')
-var baseWebpackConfig = require('./webpack.base.conf')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
-var env = config.build.env
-var CopyWebpackPlugin = require('copy-webpack-plugin')
-var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-var Path = require('path')
-var PrerenderSpaPlugin = require('prerender-spa-plugin')
-var moment = require('moment-timezone');
+var path = require("path");
+var utils = require("./utils");
+var webpack = require("webpack");
+var config = require("../config");
+var merge = require("webpack-merge");
+var baseWebpackConfig = require("./webpack.base.conf");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var env = config.build.env;
+var CopyWebpackPlugin = require("copy-webpack-plugin");
+var OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+var Path = require("path");
+var PrerenderSPAPlugin = require("prerender-spa-plugin");
+var moment = require("moment-timezone");
 
-
-var cwd = (file) => {
-  return path.join(process.cwd(), file || '')
-}
+var cwd = file => {
+  return path.join(process.cwd(), file || "");
+};
 
 var webpackConfig = merge(baseWebpackConfig, {
   module: {
@@ -25,20 +24,20 @@ var webpackConfig = merge(baseWebpackConfig, {
       extract: true
     })
   },
-  devtool: config.build.productionSourceMap ? '#source-map' : false,
+  devtool: config.build.productionSourceMap ? "#source-map" : false,
   output: {
     path: config.build.assetsRoot,
-    filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    filename: utils.assetsPath("js/[name].[chunkhash].js"),
+    chunkFilename: utils.assetsPath("js/[id].[chunkhash].js")
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
-      'process.env': env
+      "process.env": env
     }),
     new OptimizeCssAssetsPlugin({
-       assetNameRegExp: /\.css$/
- }),
+      assetNameRegExp: /\.css$/
+    }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
@@ -46,15 +45,17 @@ var webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract css into its own file
     new ExtractTextPlugin({
-      filename: utils.assetsPath('css/[name].[contenthash].css')
+      filename: utils.assetsPath("css/[name].[contenthash].css")
     }),
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
     // see https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: config.build.index,
-      template: 'index.html',
-      bannerDate: moment().tz("America/Chicago").format("dddd, MMMM Do YYYY, h:mm:ss a"),
+      template: "index.html",
+      bannerDate: moment()
+        .tz("America/Chicago")
+        .format("dddd, MMMM Do YYYY, h:mm:ss a"),
       bannerGit: "https://github.com/ICJIA/icjia-sex-offender-task-force-v2",
       bannerTitle: "Illinois Sex Offenses and Offender Registration Task Force",
       bannerContact: "cja.irc@illinois.gov",
@@ -68,84 +69,70 @@ var webpackConfig = merge(baseWebpackConfig, {
         // https://github.com/kangax/html-minifier#options-quick-reference
       },
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-      chunksSortMode: 'dependency'
+      chunksSortMode: "dependency"
     }),
-
-
-
 
     // split vendor js into its own file
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: function (module, count) {
+      name: "vendor",
+      minChunks: function(module, count) {
         // any required modules inside node_modules are extracted to vendor
         return (
           module.resource &&
           /\.js$/.test(module.resource) &&
-          module.resource.indexOf(
-            path.join(__dirname, '../node_modules')
-          ) === 0
-        )
+          module.resource.indexOf(path.join(__dirname, "../node_modules")) === 0
+        );
       }
     }),
     // extract webpack runtime and module manifest to its own file in order to
     // prevent vendor hash from being updated whenever app bundle is updated
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'manifest',
-      chunks: ['vendor']
+      name: "manifest",
+      chunks: ["vendor"]
     }),
 
-
-// for SPA Netlify deployment
-new CopyWebpackPlugin([
-{
-  from: cwd('./build/_redirects'),
-  // to the root of dist path
-  to: './'
-}
-]),
-
-
-
-
-
-
-
-
+    // for SPA Netlify deployment
+    new CopyWebpackPlugin([
+      {
+        from: cwd("./build/_redirects"),
+        // to the root of dist path
+        to: "./"
+      }
+    ])
   ]
-})
+});
 
 if (config.build.productionGzip) {
-  var CompressionWebpackPlugin = require('compression-webpack-plugin')
+  var CompressionWebpackPlugin = require("compression-webpack-plugin");
 
   webpackConfig.plugins.push(
     new CompressionWebpackPlugin({
-      asset: '[path].gz[query]',
-      algorithm: 'gzip',
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
       test: new RegExp(
-        '\\.(' +
-        config.build.productionGzipExtensions.join('|') +
-        ')$'
+        "\\.(" + config.build.productionGzipExtensions.join("|") + ")$"
       ),
       threshold: 10240,
       minRatio: 0.8
     })
-  )
+  );
 }
 
 if (config.build.bundleAnalyzerReport) {
-  var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-  webpackConfig.plugins.push(new BundleAnalyzerPlugin())
+  var BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+    .BundleAnalyzerPlugin;
+  webpackConfig.plugins.push(new BundleAnalyzerPlugin());
 }
 
 if (config.build.prerender) {
-
-  webpackConfig.plugins.push(new PrerenderSpaPlugin(
-      // Absolute path to compiled SPA
-      Path.join(__dirname, '../dist'),
-      // List of routes to prerender
-      [ '/' ]
-    ))
+  webpackConfig.plugins.push(
+    new PrerenderSPAPlugin({
+      // Required - The path to the webpack-outputted app to prerender.
+      staticDir: path.join(__dirname, "../dist"),
+      // Required - Routes to render.
+      routes: ["/"]
+    })
+  );
 }
 
-module.exports = webpackConfig
+module.exports = webpackConfig;
